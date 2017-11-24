6dd7c2b21165d375c3cb723e22d041d9d628612c
Devine Lu Linvega <aliceffekt@gmail.com>
2017-11-25 10:42:13 +1300

    Improved Runic

--- scripts/core/runic.js
+++ scripts/core/runic.js
@@ -4,8 +4,8 @@ function Runic(raw)
 
   this.runes = {
     "&":{tag:"p"},
-    "-":{tag:"list",stash:true},
-    "#":{tag:"code",stash:true},
+    "-":{tag:"list",sub:"ln",stash:true},
+    "#":{tag:"code",sub:"ln",stash:true},
     "?":{tag:"note"},
     ":":{tag:"info"},
     "*":{tag:"h2"},
@@ -15,14 +15,6 @@ function Runic(raw)
     "»":{tag:"tr",sub:"th",rep:true},
     ">":{tag:""}
   }    
-
-  this.format = function(r,html)
-  {
-    if(!this.runes[r]){ return "Unknown rune:("+r+")"; }
-    var rune = this.runes[r];
-    var result = "<"+rune.tag+">"+html+"</"+rune.tag+">";
-    return result
-  }
 
   this.markup = function(html)
   {
@@ -42,20 +34,56 @@ function Runic(raw)
 
   this.parse = function(raw = this.raw)
   {
+    if(!raw){ return ""; }
+
     var html = "";
     var lines = raw;
-    if(!raw){ return "";}
     var lines = !Array.isArray(raw) ? raw.split("\n") : raw;
 
     for(id in lines){
-      var rune = lines[id].substr(0,1);
+      var rune = this.runes[lines[id].substr(0,1)];
       var trail = lines[id].substr(1,1);
       var line = this.markup(lines[id].substr(2));
       if(!line || line.trim() == ""){ continue; }
+      if(!rune){ return "Unknown rune:("+rune+")"; }
       if(trail != " "){ console.warn("Runic","Non-rune["+trail+"] at:"+id+"("+line+")"); continue; }
-      html += this.format(rune,line);
+      html += this.render(line,rune);
     }
+    html += this.render();
     return html;
+  }
+
+  // Render
+
+  this.stash = [];
+  this.prev = null;
+
+  this.render = function(line = "",rune = null)
+  {
+    // Append to Stash
+    if(this.stash.length > 0){
+      if(rune && this.stash[0].rune.tag == rune.tag){
+        this.stash.push({line:line,rune:rune});return "";
+      }
+      else{
+        var print = this.pop_stash(); this.stash = []; return print;
+      }
+    }
+    // New Stash
+    if(rune && rune.stash && this.stash.length == 0){
+      this.stash.push({line:line,rune:rune}); return "";
+    }
+    // Default
+    return rune ? "<"+rune.tag+">"+line+"</"+rune.tag+">" : "";
+  }
+
+  this.pop_stash = function(stash = this.stash)
+  {
+    var html = ""
+    for(id in stash){
+      html += "<"+stash[0].rune.sub+">"+stash[id].line+"</"+stash[0].rune.sub+">\n";
+    }
+    return "<"+stash[0].rune.tag+">"+html+"</"+stash[0].rune.tag+">";
   }
 
   this.html = this.parse(raw);
