9456df4b8e838e1087c73d57a0b5fbdf01aaec0d
Devine Lu Linvega <aliceffekt@gmail.com>
2017-11-25 13:48:43 +1300

    Added the monitors

--- scripts/corpse/log.js
+++ scripts/corpse/log.js
@@ -3,6 +3,8 @@ function Log(list)
   this.list = list;
   this.term = this.list.term;
   this.time = new Desamber(this.list.date);
+  this.value = parseInt(this.list.code.slice(3))
+  this.sector = ["misc","audio","visual","research"][parseInt(this.list.code.slice(2).substr(0,1))]
 
   this.photo = this.list.pict ? parseInt(this.list.pict) : null;
 }
--- scripts/corpse/monitor.js
+++ scripts/corpse/monitor.js
@@ -1,10 +1,53 @@
 function Monitor(logs)
 {
-  this.el = document.createElement('yu');
+  this.el = document.createElement('yu'); this.el.id = "monitor"
+  this.lod = 75.0;
+  this.height = 50;
+  this.seg = [];
+
+  var i = 0;
+  while(i <= this.lod){
+    this.el.appendChild(this.seg[i] = document.createElement('bar'))
+    this.seg[i].style.left = (i*2)+"px";
+    i += 1;
+  }
 
   this.update = function(logs)
   {
-    console.log(logs[0].time.ago)
+    var segments = this.parse(logs);
+
+    // Find Max
+    var max = 0;
+    for(id in segments){
+      max = segments[id] > max ? segments[id] : max;
+    }
+    // Draw
+    var i = 0
+    while(i < this.lod){
+      var s = (segments[i]/max);
+      this.seg[i].style.height = s > 0 ? parseInt(s * this.height)+"px" : "1px";
+      i += 1
+    }
+  }
+
+  this.parse = function(logs)
+  {
+    var segments = {};
+    var range = {from:logs[logs.length-1].time.ago,to:logs[0].time.ago};
+    for(id in logs){
+      var log = logs[id];
+      var pos = (log.time.ago - range.to)/(range.from - range.to)
+      var level = pos * this.lod;
+      var level_prev = Math.floor(level)
+      var level_next = Math.ceil(level)
+      var distr_prev = level_next - level
+      var distr_next = 1 - distr_prev
+      if(!segments[level_prev]){ segments[level_prev] = 0; }
+      if(!segments[level_next]){ segments[level_next] = 0; }
+      segments[level_prev] += parseFloat(log.value) * distr_prev
+      segments[level_next] += parseFloat(log.value) * distr_next
+    }
+    return segments;
   }
 }
 
