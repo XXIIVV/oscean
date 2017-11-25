7d25c267357700097be55051b1b61bb7b3e513d8
Devine Lu Linvega <aliceffekt@gmail.com>
2017-11-26 09:12:54 +1300

    Fixed log value

--- scripts/corpse/log.js
+++ scripts/corpse/log.js
@@ -7,8 +7,9 @@ function Log(list)
   this.code = this.list.code;
   this.rune = this.code.substr(0,1);
   this.sector = ["misc","audio","visual","research"][parseInt(this.code.substr(2,1))]
-  this.value = parseInt(this.code.slice(3,1))
+  this.value = parseInt(this.code.substr(3,1))
   
+
   this.photo = this.list.pict ? parseInt(this.list.pict) : null;
 
   this.is_featured = this.rune == "!";
