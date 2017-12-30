03c73dee93fc077d45e7b98b4b4795c49391642d
Devine Lu Linvega <aliceffekt@gmail.com>
2017-12-31 11:44:51 +1300

    *

--- scripts/dict/lexicon.js
+++ scripts/dict/lexicon.js
@@ -1540,6 +1540,17 @@ THE BOOKS OF BABEL
 PHILIPP TRAUM
   UNDE : Notes
   BREF : {{Philipp Traum}} notes on objectivity and realism.
+  LONG
+    & Ontological realism holds that physical objects exit independantly of our own minds, while epistemological realism is the view that statements are true, or false, independantly of wether we believe them to be true, or false.
+    & Aesthetic realism holds beauty to be a real property of works of art, there to be discovered by the discerning viewer.
+    > <mini>Adapted from Baggini & Fosl</mini>
+    * NO.44
+    & In a little while you will be alone in shoreless space, to wander its limitless solitudes without friend or comrade forever - for you will remain a thought, the only existent thought, and by your nature inextinguishable, indestructible.
+    > <mini>Mark Twain's {{the mysterious Stranger|http://en.wikipedia.org/wiki/The_Mysterious_Stranger}}</mini>
+    * OBJECTIVE BEINGS
+    & Every block of stone has a statue inside it and it is the task of the sculptor to discover it. I saw the angel in the marble and carved until I set him free.
+    > <mini>Michelangelo {{Lettera a messer Benedetto Varchi|https://it.wikisource.org/wiki/Lettera_a_messer_Benedetto_Varchi}}</mini>
+
   TYPE : Docs
 
 NEEDLES
