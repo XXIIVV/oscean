24f126be95aa2cdbefe3c431a7aa77f41f16d24e
Devine Lu Linvega <aliceffekt@gmail.com>
2018-03-15 10:18:20 +1300

    Made the clock clickable.

--- scripts/graph.js
+++ scripts/graph.js
@@ -52,7 +52,7 @@ function graph()
     Ø("footer").create({x:6,y:11},DomNode,"yu",`
       <wr>
     <a href="https://twitter.com/neauoire" class="icon twitter external"></a><a href="https://github.com/neauoire" class="icon github external"></a><a href="Rotonde" class="icon rotonde"></a>
-    <yu id="clock">${new Clock().svg(35,35)}</yu><a onclick="Ø('query').bang('devine lu linvega')">Devine Lu Linvega</a> © 06I04—${new Date().desamber()}<br>BY-NC-SA 4.0 <a style="color:#ccc" onclick='Ø("query").bang("Clock")'>${new Clock()}</a><hr></wr>`),
+    <yu id="clock" onclick='Ø("query").bang("Clock")'>${new Clock().svg(35,35)}</yu><a onclick="Ø('query').bang('devine lu linvega')">Devine Lu Linvega</a> © 06I04—${new Date().desamber()}<br>BY-NC-SA 4.0 <t style="color:#ccc"'>${new Clock()}</t><hr></wr>`),
   ])
 
   // Operation
