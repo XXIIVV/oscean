29b9feb3cc993b6cf5f2cba435bba064f807ddd8
Devine Lu Linvega <aliceffekt@gmail.com>
2018-06-16 16:46:46 +1200

    *

--- scripts/graph.js
+++ scripts/graph.js
@@ -43,7 +43,7 @@ function graph()
     Ø("footer").create({x:6,y:8},DomNode,"yu",`
       <wr>
     <a href="https://twitter.com/neauoire" class="icon twitter external"></a><a href="https://github.com/neauoire" class="icon github external"></a><a href="http://webring.xxiivv.com/#random" class="icon rotonde"></a>
-    <yu id="clock" onclick='Ø("query").bang("Clock")'>${new Entaloneralie().svg(35,35)}</yu><a onclick="Ø('query').bang('devine lu linvega')">Devine Lu Linvega</a> © 06I04—${new Date().desamber()}<br>BY-NC-SA 4.0 <t style="color:#ccc"'>${new Clock()}</t><hr></wr>`),
+    <yu id="clock" onclick='Ø("query").bang("Time")'>${new Entaloneralie().svg(35,35)}</yu><a onclick="Ø('query').bang('devine lu linvega')">Devine Lu Linvega</a> © 06I04—${new Date().desamber()}<br>BY-NC-SA 4.0 <t style="color:#ccc"'>${new Clock()}</t><hr></wr>`),
   ])
 
   // Operation
