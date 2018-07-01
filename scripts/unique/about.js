dc70973f82b9d50001756f815787040e31f3a737
Devine Lu Linvega <aliceffekt@gmail.com>
2018-07-01 12:36:02 +1200

    *

--- scripts/unique/about.js
+++ scripts/unique/about.js
@@ -42,7 +42,7 @@
   <p>The {{Riven Engine|Riven}} is designed to run without a serving platform, using only {{front-end files|http://github.com/XXIIVV/Oscean}} written in an {{unobfuscated format|Oscean}} — With the hope that little or {{no migration|https://www.gwern.net/About#long-site}} will ever be required.</p>
   <p>So far, roughly {*${parseInt(new Horaire(q.tables.lexicon.OSCEAN.logs).sum)} hours*}(over ${q.tables.lexicon.OSCEAN.logs.length} days) were invested in the construction of {{Oscean}}, which is a considerable investment of time, enough that one might wonder if it is actually worth it. There is no singular project that had more {{impact|Aesthetics}} on my work than the usage and creation of this {{application|Nataniev}}.</p>
   ${new Runic(": Oscean",q.tables)}
-  <p>{{Desamber}} Time is the current {{Time Format|Time}} used on this wiki, the current time is <b>{{$desamber}}</b> <code class='inline'>{{$clock}}</code>, visit the {{Clock}} for more details. The reason for this use of un {{unconventional|https://en.wikipedia.org/wiki/Decimal_time#See_also}} time is the perdictable {{2-weeks long periods|Desamber}} of each month, ideal for the {{sprint format|https://en.wikipedia.org/wiki/Agile_software_development}}.</p>
+  <p>{{Desamber}} is the {{Time Format|Time}} used on this wiki, the current time is <b>{{$desamber}}</b> <code class='inline'>{{$clock}}</code>, visit the {{Clock}} for more details. The reason for using an {{unconventional|https://en.wikipedia.org/wiki/Decimal_time#See_also}} time is the perdictable {{2-weeks long length|Desamber}} of each month, ideal for the {{sprint periods|https://en.wikipedia.org/wiki/Agile_software_development}}.</p>
   `.to_markup()
 
   //
