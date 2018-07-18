0328cb16181b41eee9cb6a35a848b90c80ae1372
Devine Lu Linvega <aliceffekt@gmail.com>
2018-07-19 09:35:40 +1200

    *

--- scripts/viz/status.js
+++ scripts/viz/status.js
@@ -12,7 +12,7 @@ function StatusViz(logs,settings = {})
       var offset = log.time.offset();
       if(offset > 0){ continue; }
       if(offset > -14){ h.now.push(log); continue; }
-      if(offset > -48){ h.before.push(log); continue; }
+      if(offset > -24){ h.before.push(log); continue; }
       break;
     }
     return h
@@ -29,16 +29,16 @@ function StatusViz(logs,settings = {})
       <tr>
         <th>
           <t class='display'>
-            <t class='value'>${now.sum.toFixed(2)}</t>
+            <t class='value'>${now.sum.toFixed(1)}</t>
             <t class='offset'>${this.offset(now.sum,before.sum)}</t>
-            <t class='unit'>hour</t>
+            <t class='unit'>hours</t>
           </t>
         </th>
         <th>
           <t class='display'>
-            <t class='value'>${now.balance.toFixed(2)}</t>
-            <t class='offset'>${this.offset(now.balance,before.balance)}</t>
-            <t class='unit'>blnc</t>
+            <t class='value'>${now.fh.toFixed(2)}</t>
+            <t class='offset'>${this.offset(now.fh,before.fh)}</t>
+            <t class='unit'>hours/day</t>
           </t>
         </th>
         <th>
@@ -68,10 +68,10 @@ function StatusViz(logs,settings = {})
   {
     return `
     <style>
-      #content .graph.status { background:transparent; padding:0px; width:790px}
+      #content .graph.status { background:transparent; padding:0px; width:730px}
       #content .graph.status th { font-size:40px; font-family:'archivo_light';line-height:40px; }
       #content .graph.status tr > * { padding:5px 0px}
-      #content .graph.status th t.display { display:inline-block; border-bottom:1.5px solid black}
+      #content .graph.status th t.display { display:inline-block; border-bottom:1.5px solid black; width:180px; padding-bottom:15px}
       #content .graph.status th t.display t.value {}
       #content .graph.status th t.display t.unit { font-size:12px; font-family:'input_mono_regular';}
       #content .graph.status th t.display t.offset { font-size:12px; font-family:'input_mono_medium'; text-transform:uppercase; position:absolute; margin-top:-7.5px}
