e48f39506446bf79012d38f40093d9a9a4405ee8
Devine Lu Linvega <aliceffekt@gmail.com>
2018-08-12 08:17:01 +1200

    Cleaned up lifeline graph

--- scripts/viz/lifeline.js
+++ scripts/viz/lifeline.js
@@ -9,17 +9,7 @@ function LifelineViz(terms)
   this.position = function(time, years = 10)
   {
     var ratio = 1+(time.offset/(365*years)); // Over the last 10 years
-    var x = clamp(ratio * width,0,width)
-
-    return step(x,cell+1) + (cell/2) + padding;
-  }
-
-  this._event = function(log,y)
-  {
-    var html = ""
-
-    html += log.task == "release" ? `<circle class='release' cx="${this.position(log.time)}" cy="${y}" r="${(cell/5)-0.5}"></circle>` : '<circle cx="${this.position(log.time)}" cy="${y}" r="${cell/5}"></circle>'
-    return html
+    return step(clamp(ratio * width,0,width),cell+1) + (cell/2) + padding;
   }
 
   this._project = function(id,term)
@@ -32,7 +22,7 @@ function LifelineViz(terms)
     var y = (id * (cell+1))+(cell/2)
 
     // Clickable
-    html += `<rect x='0' y='${y}' width='${width+padding+cell}' height='${cell}' style='fill:black; stroke:none'/>`
+    html += `<rect x='0' y='${y}' width='${width+padding+cell}' height='${cell}' style='fill:black; stroke:none' onclick="Ø('query').bang('${term.name}')"/>`
 
     for(var id in term.logs){
       var log = term.logs[id];
@@ -47,12 +37,18 @@ function LifelineViz(terms)
     html += `<line x1='${(cell/2)+padding}' y1='${y}' x2='${this.position({offset:0})}' y2='${y}' class='bg' />`
 
     if(span.release){
-      var w = this.position(span.release.time) - this.position(span.from.time) - 1
-      html += `<rect class='development' x='${this.position(span.from.time)-(cell/2)}' y='${y-(cell/2)}' width='${clamp(w,cell,width)}' height='${cell}' rx="2" ry="2" />`
-      w = this.position(span.to.time) - this.position(span.release.time) - 1
-      html += `<rect class='maintenance' x='${this.position(span.release.time)-(cell/2)}' y='${y-(cell/2)}' width='${clamp(w,cell,width)}' height='${cell}' rx="2" ry="2" />`
-      w = width - this.position(span.to.time) + padding + (cell*1.5)
-      html += `<rect class='released' x='${this.position(span.to.time)-(cell/2)}' y='${y-(cell/2)}' width='${clamp(w,cell,width)}' height='${cell}' rx="2" ry="2" />`
+      var development = {w:this.position(span.release.time) - this.position(span.from.time) - 1,x:this.position(span.from.time)-(cell/2)}
+      var release = {w:this.position(span.to.time) - this.position(span.release.time) - 1,x:this.position(span.release.time)-(cell/2)}
+      var maintenance = {w:width - this.position(span.to.time) + padding + (cell*1.5),x:this.position(span.to.time)-(cell/2)}
+
+      html += `<rect class='development' x='${development.x}' y='${y-(cell/2)}' width='${clamp(development.w,cell,width)}' height='${cell}' rx="2" ry="2" />`
+      html += `<rect class='release' x='${release.x}' y='${y-(cell/2)}' width='${clamp(release.w,cell,width)}' height='${cell}' rx="2" ry="2" />`
+      
+      if(maintenance.x == release.x){
+        maintenance.x += cell+1;
+        maintenance.w -= cell+1;
+      }
+      html += `<rect class='maintenance' x='${maintenance.x}' y='${y-(cell/2)}' width='${clamp(maintenance.w,cell,width)}' height='${cell}' rx="2" ry="2" />`
     }
     else{
       var w = this.position(span.to.time) - this.position(span.from.time) - 1
@@ -67,9 +63,6 @@ function LifelineViz(terms)
   this.draw = function()
   {
     var html = ""
-
-    var cell = parseInt(700/52);
-
     var unsorted = []
 
     for(var id in this.terms){
@@ -87,9 +80,22 @@ function LifelineViz(terms)
       i += 1;
     }
 
-    html += `<text class='bold' x='0' y='-15'>10 Years ago</text><text class='right bold' x='${width+padding+cell}' y='-15'>Today</text>`
+    var h = (sorted.length * cell)+(cell*10);
 
-    return `<svg class='graph timeline' style='height:${(sorted.length * cell)+(cell*10)}px; width:730px'>${html}</svg>`
+    html += `
+    <text class='bold' x='0' y='-15'>Projects</text>
+    <text class='bold' x='${padding+(cell/2)}' y='-15'>10 Years ago</text>
+    <text class='right bold' x='${width+padding+cell}' y='-15'>Today</text>
+    <rect class="development" x="${padding}" y="${h-(cell*2)-cell}" width="${cell}" height="${cell}" rx="2" ry="2" title="17O11"></rect>
+    <text x='${padding+(cell*2)}' y='${h-(cell*1.25)-cell}' class='bold'>Development</text>
+    <rect class="release" x="${padding+(cell*9)}" y="${h-(cell*2)-cell}" width="${cell}" height="${cell}" rx="2" ry="2" title="17O11"></rect>
+    <text x='${padding+(cell*2)+(cell*9)}' y='${h-(cell*1.25)-cell}' class='bold'>Release</text>
+    <rect class="maintenance" x="${padding+(cell*18)}" y="${h-(cell*2)-cell}" width="${cell}" height="${cell}" rx="2" ry="2" title="17O11"></rect>
+    <text x='${padding+(cell*2)+(cell*18)}' y='${h-(cell*1.25)-cell}' class='bold'>Maintenance</text>
+    <rect class="unreleased" x="${padding+(cell*27)}" y="${h-(cell*2)-cell}" width="${cell}" height="${cell}" rx="2" ry="2" title="17O11"></rect>
+    <text x='${padding+(cell*2)+(cell*27)}' y='${h-(cell*1.25)-cell}' class='bold'>Unreleased</text>`
+
+    return `<svg class='graph timeline' style='height:${h}px; width:728px'>${html}</svg>`
   }
 
   this.style = function()
@@ -99,20 +105,16 @@ function LifelineViz(terms)
       svg.graph.timeline { margin-bottom:30px; padding-top:30px}
       svg.graph.timeline line { stroke-width:1.5; stroke:#fff }
       svg.graph.timeline line.bg { stroke-width:1.5; stroke:#333 }
-      svg.graph.timeline circle { stroke:none; fill:white }
-      svg.graph.timeline circle.release { stroke:white; stroke-width:1.5; fill:none; }
       svg.graph.timeline rect { stroke:none }
       svg.graph.timeline rect.development { fill:#72dec2 }
-      svg.graph.timeline rect.maintenance { fill:#51a196 }
-      svg.graph.timeline rect.released { fill:#316067 }
+      svg.graph.timeline rect.release { fill:#51a196 }
+      svg.graph.timeline rect.maintenance { fill:#316067 }
       svg.graph.timeline rect.unreleased { fill:#333 }
-      svg.graph.timeline text { font-family:'archivo_medium'; stroke:none; fill:white; font-size:11px }
+      svg.graph.timeline text { font-family:'archivo_medium'; stroke:none; fill:white; font-size:11px;text-anchor:start }
       svg.graph.timeline text.unreleased { fill:#999 !important}
       svg.graph.timeline text.right { text-anchor:end}
       svg.graph.timeline text.bold { font-family:'archivo_bold'}
       svg.graph.timeline g { cursor:pointer}
-      svg.graph.timeline g:hover line.activity { stroke:#72dec2 !important}
-      svg.graph.timeline g:hover circle { fill:#72dec2 !important}
       svg.graph.timeline g:hover text { fill:#72dec2 !important}
     </style>
     `
