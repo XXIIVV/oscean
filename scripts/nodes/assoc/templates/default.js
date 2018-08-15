cdf69175ffbcd2796c03959f3df3df6e1889ac1c
Devine Lu Linvega <aliceffekt@gmail.com>
2018-08-15 20:15:28 +1200

    Minor changes

--- scripts/nodes/assoc/templates/default.js
+++ scripts/nodes/assoc/templates/default.js
@@ -40,7 +40,7 @@ function DefaultTemplate(id,rect,...params)
     for(id in term.children){
       var child = term.children[id];
       html += `
-      ${child.featured_log ? `<a onclick='Ø("query").bang("${child.name}")'><img src="media/diary/${child.featured_log.photo}.jpg"/></a><hs>— ${child.bref().to_markup()}</hs>` : `<h2>${child.name.capitalize()}</h2><hs>${child.bref()}</hs>`.to_markup()}
+      ${child.featured_log ? `<a onclick='Ø("query").bang("${child.name}")'><img src="media/diary/${child.featured_log.photo}.jpg"/></a><hs>${child.bref().to_markup()}</hs>` : `<h2>${child.name.capitalize()}</h2><hs>${child.bref()}</hs>`.to_markup()}
       ${child.long(q.tables)}`
     }
     return html
