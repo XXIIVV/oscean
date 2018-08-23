1a380f4eb0ca1bb5e3837749b37405a9d6a9c14a
Devine Lu Linvega <aliceffekt@gmail.com>
2018-08-24 10:11:23 +1200

    Basic index

--- scripts/riven/nodes/assoc/templates/index.js
+++ scripts/riven/nodes/assoc/templates/index.js
@@ -4,9 +4,56 @@ function IndexTemplate(id,rect,...params)
 
   this.glyph = NODE_GLYPHS.element
   
+  this.entries = {}
+
+  this.add = function(t,source)
+  {
+    var term = t.toLowerCase().trim();
+    if(!term||term == ""){ return; }
+    if(!this.entries[term]){ this.entries[term] = []; }
+    if(this.entries[term].indexOf(source) > -1){ return; }
+    this.entries[term].push(source)
+  }
+
+  this.collect = function(q)
+  {
+    for(var id in q.tables.lexicon){
+      var term = q.tables.lexicon[id]
+      this.add(term.name,"lexicon")
+    }
+
+    for(var id in q.tables.glossary){
+      var list = q.tables.glossary[id]
+      this.add(list.name,"glossary")
+      for(var i in list.data){
+        this.add(i,"glossary")
+      }
+    }
+
+    for(var id in q.tables.horaire){
+      this.add(q.tables.horaire[id].name,"horaire")
+    }
+
+    this.entries;
+  }
+
   this.answer = function(q)
   {
-    console.log(q.tables)
-    return "hello"
+    this.collect(q);
+
+    var sortable = Object.keys(this.entries).sort();
+
+    var prev = ""
+    var html = ""
+    for(id in sortable){
+      var lead = sortable[id].substr(0,1)
+      if(parseInt(lead) > 0){ continue; }
+      if(prev != lead){
+        html += `<h2>${lead}</h2>`
+      }
+      html += `<ln>{(${sortable[id].capitalize()})} — ${this.entries[sortable[id]]}</ln>`.to_curlic()
+      prev = lead
+    }
+    return `<list>${html}</list>`
   }
 }
