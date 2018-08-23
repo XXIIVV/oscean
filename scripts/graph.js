a3da1feccb1fcf08f6703adc1fa0eb36c89c05b4
Devine Lu Linvega <aliceffekt@gmail.com>
2018-08-23 15:48:43 +1200

    Improved docs

--- scripts/graph.js
+++ scripts/graph.js
@@ -106,3 +106,14 @@ function graph()
 
   Ø("query").bang()
 }
+
+
+// Tools
+
+String.prototype.replace_all = function(search, replacement){ return `${this}`.split(search).join(replacement); };
+String.prototype.capitalize = function(){ return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase(); }
+String.prototype.to_url = function(){ return this.toLowerCase().replace(/ /g,"+").replace(/[^0-9a-z\+\:\-\.\/]/gi,"").trim(); }
+String.prototype.to_path = function(){ return this.toLowerCase().replace(/\+/g,".").replace(/ /g,".").replace(/[^0-9a-z\.\-]/gi,"").trim(); }
+String.prototype.to_entities = function(){ return this.replace(/[\u00A0-\u9999<>\&]/gim, function(i) { return `&#${i.charCodeAt(0)}`; }); }
+String.prototype.to_rss = function(){ return this.replace(/\</g,"&lt;").replace(/\>/g,"&gt;") }
+String.prototype.count=function(c) { var r = 0, i = 0; for(i;i<this.length;i++)if(this[i]==c) r++; return r; }
--- scripts/lib/curlic.js
+++ scripts/lib/curlic.js
@@ -33,8 +33,23 @@ function Curlic(text = "",force_external = false)
     return `<a href='${href}' title='${target}' onclick="${click}" class='${className}' target='${view}'>${name ? name : target}</a>`
   }
 
+  function evaluate(s,t)
+  {
+    try{
+      return `${eval(t.substr(1,t.length-2))}`  
+    }
+    catch(err){
+      console.warn("Cannot eval",t); return t
+    }
+  }
+
   function parse(s)
   {
+    // Eval
+    if(s.match(/\[.*\]/g)){
+      s = evaluate(s,s.match(/\[.*\]/g)[0])
+    }
+
     // Wrap
     for(var ch in runes){
       var rune = runes[ch];
@@ -46,7 +61,6 @@ function Curlic(text = "",force_external = false)
     if(s.match(/\(.*\)/g)){
       s = link(s,s.match(/\(.*\)/g)[0])
     }
-    
     return s
   }
 
@@ -67,11 +81,4 @@ function Curlic(text = "",force_external = false)
   }
 }
 
-String.prototype.replace_all = function(search, replacement){ return `${this}`.split(search).join(replacement); };
-String.prototype.capitalize = function(){ return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase(); }
 String.prototype.to_curlic = function(force_external){ return `${new Curlic(this,force_external)}`; }
-String.prototype.to_url = function(){ return this.toLowerCase().replace(/ /g,"+").replace(/[^0-9a-z\+\:\-\.\/]/gi,"").trim(); }
-String.prototype.to_path = function(){ return this.toLowerCase().replace(/\+/g,".").replace(/ /g,".").replace(/[^0-9a-z\.\-]/gi,"").trim(); }
-String.prototype.to_entities = function(){ return this.replace(/[\u00A0-\u9999<>\&]/gim, function(i) { return `&#${i.charCodeAt(0)}`; }); }
-String.prototype.to_rss = function(){ return this.replace(/\</g,"&lt;").replace(/\>/g,"&gt;") }
-String.prototype.count=function(c) { var r = 0, i = 0; for(i;i<this.length;i++)if(this[i]==c) r++; return r; }
