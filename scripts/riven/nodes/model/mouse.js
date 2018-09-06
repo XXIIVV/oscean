22a9849de566687b975c245623439d864f0feeb7
Devine Lu Linvega <aliceffekt@gmail.com>
2018-09-06 15:53:22 +1200

    Implemented tab open

--- scripts/riven/nodes/model/mouse.js
+++ scripts/riven/nodes/model/mouse.js
@@ -9,11 +9,21 @@ function MouseNode(id,rect)
 
   this.click = function(e)
   {
+    let in_tab = e.ctrlKey || e.shiftKey || e.metaKey || 2 === e.which || (e.button && e.button == 1)
+
     if(e.target.className == 'external'){ return; }
 
     let el = e.target.getAttribute('data-goto') ? e.target : e.target.parentNode.getAttribute('data-goto') ? e.target.parentNode : null;
 
     if(!el){ return; }
+    
+    if(in_tab){ 
+      let win = window.open(`https://wiki.xxiivv.com/${el.getAttribute('data-goto')}`, '_blank');
+      win.focus();
+      return; 
+    }
+
+    
 
     Ø('query').bang(el.getAttribute('data-goto'))
     e.preventDefault();
