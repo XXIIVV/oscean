26663189569d915eaf4f9369a2874dc334dc5cb9
Devine Lu Linvega <aliceffekt@gmail.com>
2018-10-21 20:04:05 +1200

    *

--- scripts/graph.js
+++ scripts/graph.js
@@ -23,7 +23,7 @@ RIVEN.create = (append = true) => {
   ], 'router', 'router')
 
   Ø('assoc').create({ x: 28, y: 0 }, lib.Mesh, [
-    Ø('build').create({ x: 3, y: 0 }, lib.Build),
+    Ø('build').create({ x: 6, y: 0 }, lib.Build),
     Ø('_header').create({ x: 0, y: 4 }, lib.Header),
     Ø('_sidebar').create({ x: 3, y: 4 }, lib.Sidebar),
     Ø('_navi').create({ x: 9, y: 4 }, lib.Navi),
@@ -36,23 +36,23 @@ RIVEN.create = (append = true) => {
   ], 'build', 'build')
 
   Ø('client').create({ x: 50, y: 0 }, lib.Mesh, [
-    Ø('view').create({ x: 0, y: 2 }, lib.Document, append),
-    Ø('terminal').create({ x: 6, y: 6 }, lib.Terminal),
-    Ø('header').create({ x: 0, y: 6 }, lib.Dom),
-    Ø('photo').create({ x: 0, y: 10 }, lib.Photo, 'photo'),
-    Ø('logo').create({ x: 6, y: 10 }, lib.Dom, 'a', null, { 'data-goto': 'home', href: '#home' }),
-    Ø('menu').create({ x: 3, y: 10 }, lib.Dom),
-    Ø('search').create({ x: 0, y: 14 }, lib.Input),
-    Ø('activity').create({ x: 3, y: 14 }, lib.Dom, 'ul'),
-    Ø('info').create({ x: 9, y: 10 }, lib.Dom),
-    Ø('glyph').create({ x: 9, y: 14 }, lib.Path),
-    Ø('title').create({ x: 6, y: 14 }, lib.Dom),
-    Ø('core').create({ x: 12, y: 6 }, lib.Dom),
-    Ø('content').create({ x: 12, y: 10 }, lib.Dom),
-    Ø('sidebar').create({ x: 15, y: 10 }, lib.Dom),
-    Ø('navi').create({ x: 18, y: 10 }, lib.Dom, 'ul'),
-    Ø('footer').create({ x: 21, y: 6 }, lib.Dom),
-    Ø('credits').create({ x: 21, y: 10 }, lib.Dom, 'div', `
+    Ø('view').create({ x: 0, y: 0 }, lib.Document, append),
+    Ø('terminal').create({ x: 6, y: 4 }, lib.Terminal),
+    Ø('header').create({ x: 0, y: 4 }, lib.Dom),
+    Ø('photo').create({ x: 0, y: 8 }, lib.Photo, 'photo'),
+    Ø('logo').create({ x: 6, y: 8 }, lib.Dom, 'a', null, { 'data-goto': 'home', href: '#home' }),
+    Ø('menu').create({ x: 3, y: 8 }, lib.Dom),
+    Ø('search').create({ x: 0, y: 12 }, lib.Input),
+    Ø('activity').create({ x: 3, y: 12 }, lib.Dom, 'ul'),
+    Ø('info').create({ x: 9, y: 8 }, lib.Dom),
+    Ø('glyph').create({ x: 9, y: 12 }, lib.Path),
+    Ø('title').create({ x: 6, y: 12 }, lib.Dom),
+    Ø('core').create({ x: 12, y: 4 }, lib.Dom),
+    Ø('content').create({ x: 12, y: 8 }, lib.Dom),
+    Ø('sidebar').create({ x: 15, y: 8 }, lib.Dom),
+    Ø('navi').create({ x: 18, y: 8 }, lib.Dom, 'ul'),
+    Ø('footer').create({ x: 21, y: 4 }, lib.Dom),
+    Ø('credits').create({ x: 21, y: 8 }, lib.Dom, 'div', `
       <a target='_blank' href="https://twitter.com/neauoire" class="icon twitter external"></a>
       <a target='_blank' href="https://github.com/neauoire" class="icon github external"></a>
       <a target='_blank' href="http://webring.xxiivv.com/#random" class="icon rotonde"></a>
