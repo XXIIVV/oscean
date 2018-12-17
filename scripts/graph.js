febaf4a02b66dfb3d65a5019b1f75c3f5e369155
Devine Lu Linvega <aliceffekt@gmail.com>
2018-12-18 11:06:20 +1200

    Added link to Merveilles instance

--- scripts/graph.js
+++ scripts/graph.js
@@ -52,6 +52,7 @@ RIVEN.create = (append = true) => {
       <a target='_blank' href="https://twitter.com/neauoire" class="icon twitter external"></a>
       <a target='_blank' href="https://github.com/neauoire" class="icon github external"></a>
       <a target='_blank' href="http://webring.xxiivv.com/#random" class="icon rotonde"></a>
+      <a target='_blank' href="https://merveilles.town/@neauoire" class="icon merveilles external"></a>
       <a target='_blank' href="https://creativecommons.org/licenses/by-nc-sa/4.0/" class="icon cc"></a>
       <a data-goto='devine lu linvega' href='#devine+lu+linvega'>Devine Lu Linvega</a> © ${new Desamber('06I04').toString(true)}—${desamber()}
       <center><a data-goto='About' href='#About'>BY-NC-SA 4.0</a> <span style="color:#ccc"'>${neralie()}</span></center>
