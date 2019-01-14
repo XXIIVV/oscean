0d71fe82644e5bd0f4e8c2244a3e630e20a23818
Devine Lu Linvega <aliceffekt@gmail.com>
2019-01-15 11:26:22 +1200

    Fixed issue with status

--- scripts/types/status.js
+++ scripts/types/status.js
@@ -3,15 +3,16 @@
 function Status (logs) {
   this.toString = function () {
     const len = 14
+    const today = new Desamber()
     const now = new Horaire(logs.filter(__onlyCurrentMonth))
     const average = new Horaire(logs.filter(__onlyLast365))
     const elapsed = parseInt(now.range.to.time.d)
-
+    
     return `
     <div class='status'>
       <ul>
         <li><b style='color:#72dec2'>Devine Lu Linvega</b></li>
-        <li style='color:#666; border-bottom:1.5px solid #333; padding-bottom:10px; margin-bottom:10px'>${now.range.from.time}—${now.range.to.time}, ${14 - now.range.to.time.d} days left</li>
+        <li style='color:#666; border-bottom:1.5px solid #333; padding-bottom:10px; margin-bottom:10px'>${now.range.from.time}—${today}, ${14 - today.d} days left</li>
         ${_item('Stamina', (now.fh * elapsed), (average.fh * len))}
         ${_item('Productivity', (now.ch * elapsed), average.ch * len)}
         ${_item('Focus', (now.os * elapsed), average.os * len)}
