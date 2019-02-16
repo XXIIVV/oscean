a101bbb56371d22f324072e861d2273c50b3ec9f
Devine Lu Linvega <aliceffekt@gmail.com>
2019-02-16 10:10:53 +0900

    *

--- scripts/lib/neralie.js
+++ scripts/lib/neralie.js
@@ -1,17 +1,18 @@
 'use strict'
 
-function neralie (date = new Date()) {
-  const e = new Date(date)
-  const ms = e - date.setHours(0, 0, 0, 0)
+function neralie (d = new Date(), e = new Date(d)) {
+  const ms = e - d.setHours(0, 0, 0, 0)
   const val = (ms / 8640 / 10000).toFixed(6)
   return `${val.substr(2, 3)}:${val.substr(5, 3)}`
 }
 
-function Neralie (t = neralie()) {
-  this.beat = t.split(':')[0]
-  this.pulse = t.split(':')[1]
+function Neralie (t = neralie) {
+  this.toInteger = function (d = new Date(), e = new Date(d)) {
+    const ms = e - d.setHours(0, 0, 0, 0)
+    return (ms / 8640) * 100
+  }
 
   this.toString = function () {
-    return t
+    return `${neralie()}`
   }
 }
