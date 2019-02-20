42a78664962da9430a585207cd7f06db494d76f4
Devine Lu Linvega <aliceffekt@gmail.com>
2019-02-20 09:29:18 +0900

    LXTWT test

--- scripts/riven/nodes/services/lxtxt.js
+++ scripts/riven/nodes/services/lxtxt.js
@@ -6,7 +6,8 @@ RIVEN.lib.Lxtxt = function LxtxtNode (id, rect) {
   this.glyph = 'M65,65 L65,65 L245,65 L245,245 L65,245 Z M65,125 L65,125 L245,125 M95,95 L95,95 L95,95 '
 
   function _template (acc, name) {
-    return `${acc}${name}\t${2}\n`
+    const term = Ø('database').cache.lexicon[name]
+    return term.logs.length > 20 ? `${acc}${name}\t${term.bref.toCurlic().stripHTML()}\n` : acc
   }
 
   this.receive = function () {
