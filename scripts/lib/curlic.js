c3a788eb9d9229ce6d279014ed90aea7e9286fbc
Devine Lu Linvega <aliceffekt@gmail.com>
2019-03-04 15:50:16 +0900

    Progress on removing curlic

--- scripts/lib/curlic.js
+++ scripts/lib/curlic.js
@@ -1,12 +1,7 @@
 'use strict'
 
 function curlic (text = '', host) {
-  const runes = {
-    '*': { tag: 'b' },
-    '_': { tag: 'i' },
-    '#': { tag: 'code' }
-  }
-
+  
   function validate (t) {
     if (t.indexOf('(') > -1 && t.indexOf(')') < 0) { console.warn(`Missing closing(:${t}`) }
     if (t.indexOf('[') > -1 && t.indexOf(']') < 0) { console.warn(`Missing closing[:${t}`) }
@@ -17,18 +12,13 @@ function curlic (text = '', host) {
     return new Heol(t, Ø('database').cache, host)
   }
 
-  function wrap (s, c, r) {
-    if (s.indexOf(c) > 0) { return s }
-    s = s.replace(c, `<${r.tag}>`).replace(c, `</${r.tag}>`)
-    return r.fn ? s.replace(s, r.fn(s)) : s
-  }
-
   function link (s, t) {
     const target = t.substr(1, t.length - 2).trim()
     const external = target.indexOf('//') > -1
     const name = s.replace(`(${target})`, '').trim()
     const location = target.toLowerCase().replace(/ /g, '+').replace(/[^0-9a-z\+\:\-\.\/]/gi, '').trim()
 
+    console.log(s,t)
     return `<a href='${external ? target : 'index.html#' + location}' target='${external ? '_blank' : '_self'}' class='${external ? 'external' : 'local'}' data-goto='${external ? '' : target}'>${name || target}</a>`
   }
 
@@ -40,11 +30,6 @@ function curlic (text = '', host) {
 
     const toLink = s.match(/\((.*)\)/g)
     if (toLink) { s = s.replace(toLink[0], '') }
-
-    for (const ch in runes) {
-      if (s.indexOf(ch) < 0) { continue }
-      s = wrap(s, ch, runes[ch])
-    }
 
     if (toLink) {
       s = link(s, toLink[0])
