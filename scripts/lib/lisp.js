140bbbc26865c1b52cc4535ad7e4365ada1feadb
Devine Lu Linvega <aliceffekt@gmail.com>
2019-05-09 20:37:09 +0900

    Optimized LISP parser

--- scripts/lib/lisp.js
+++ scripts/lib/lisp.js
@@ -4,6 +4,7 @@
 // https://github.com/maryrosecook/littlelisp
 
 function Lisp (input, lib, tables, host) {
+  const TYPES = { identifier: 0, keyword: 1, number: 2, string: 3, bool: 4 }
   const Context = function (scope, parent) {
     this.scope = scope
     this.parent = parent
@@ -53,26 +54,26 @@ function Lisp (input, lib, tables, host) {
       return interpret(input, new Context(lib))
     } else if (input instanceof Array) {
       return interpretList(input, context)
-    } else if (input.type === 'identifier') {
+    } else if (input.type === TYPES.identifier) {
       return context.get(input.value)
-    } else if (input.type === 'keyword') {
+    } else if (input.type === TYPES.keyword) {
       return host[input.value] ? host[input.value] : null
-    } else if (input.type === 'number' || input.type === 'string' || input.type === 'bool') {
+    } else if (input.type === TYPES.number || input.type === TYPES.string || input.type === TYPES.bool) {
       return input.value
     }
   }
 
   const categorize = function (input) {
     if (!isNaN(parseFloat(input))) {
-      return { type: 'number', value: parseFloat(input) }
+      return { type: TYPES.number, value: parseFloat(input) }
     } else if (input[0] === '"' && input.slice(-1) === '"') {
-      return { type: 'string', value: input.slice(1, -1) }
+      return { type: TYPES.string, value: input.slice(1, -1) }
     } else if (input[0] === ':') {
-      return { type: 'keyword', value: input.substr(1, input.length - 1) }
+      return { type: TYPES.keyword, value: input.substr(1, input.length - 1) }
     } else if (input === 'true' || input === 'false') {
-      return { type: 'bool', value: input === 'true' }
+      return { type: TYPES.bool, value: input === 'true' }
     } else {
-      return { type: 'identifier', value: input }
+      return { type: TYPES.identifier, value: input }
     }
   }
 
@@ -94,7 +95,7 @@ function Lisp (input, lib, tables, host) {
   }
 
   const tokenize = function (input) {
-    return input.split('"').map(function (x, i) { return i % 2 === 0 ? x.replace(/\(/g, ' ( ').replace(/\)/g, ' ) ') : x.replace(/ /g, '!whitespace!') }).join('"').trim().split(/\s+/).map(function (x) { return x.replace(/!whitespace!/g, ' ') })
+    return input.replace(/^\;.*\n?/gm, '').split('"').map(function (x, i) { return i % 2 === 0 ? x.replace(/\(/g, ' ( ').replace(/\)/g, ' ) ') : x.replace(/ /g, '!whitespace!') }).join('"').trim().split(/\s+/).map(function (x) { return x.replace(/!whitespace!/g, ' ') })
   }
 
   this.parse = function (input) {
