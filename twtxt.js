34e986a9feeeae50e337c65815591302dee696d0
Devine Lu Linvega <aliceffekt@gmail.com>
2019-07-29 14:25:38 +0900

    Replaced triple spaces to tabs

--- twtxt.js
+++ twtxt.js
@@ -1,10 +1,2 @@
-const fs = require('fs')
-const path = 'twtxt.txt'
-const iso = new Date().toISOString()
-const prev = fs.readFileSync(path, 'utf8')
-const entry = process.argv[2]
-
-if (fs.existsSync(path)) {
-  fs.writeFileSync(path, iso + '   ' + entry + '\n' + prev)
-  console.log(`Added entry #${prev.split('\n').length + 1}.\n> ${iso} ${entry}\n\n`)
-}
+// Decentralised, minimalist microblogging service for hackers.
+require('fs').appendFileSync('twtxt.txt', new Date().toISOString() + '\t' + process.argv[2])
