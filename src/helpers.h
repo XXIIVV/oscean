00764b3f39f44d7bb164e4b940488b013d63509a
Devine Lu Linvega <aliceffekt@gmail.com>
2020-09-15 15:13:53 -0700

    *

--- src/helpers.h
+++ src/helpers.h
@@ -250,5 +250,5 @@ fputs_rfc3339(FILE* f, time_t t)
 	fprintf(f, "%04d-%02d-%02dT%02d:%02d:%02d%c%02d:%02d",
 	        tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
 	        tm->tm_hour, tm->tm_min, tm->tm_sec,
-	        '+', 1, 0);
+	        '-', 7, 0); /* Vancouver GMT-7*/
 }
