137bf158dc6c1b8ab99e6610a69f77f6689c8edf
neauoire <aliceffekt@gmail.com>
2020-10-18 14:20:13 -0700

    Replaced logo with png

--- src/main.c
+++ src/main.c
@@ -655,7 +655,7 @@ fphtml(FILE* f, Lexicon* lex, Term* t, Journal* jou)
 	        t->bref, t->name);
 	fputs("</head>", f);
 	fputs("<body>", f);
-	fputs("<header><a href='home.html'><img src='../media/identity/xiv28.gif' alt='" NAME "' height='29'></a></header>", f);
+	fputs("<header><a href='home.html'><img src='../media/services/logo.png' alt='" NAME "' height='29'></a></header>", f);
 	fpnav(f, t);
 	fputs("<main>", f);
 	fpbanner(f, jou, t, 1);
