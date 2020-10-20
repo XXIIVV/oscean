03125f272df70bc6264862661a779e20bad63af0
neauoire <aliceffekt@gmail.com>
2020-10-19 20:56:39 -0700

    Optimization

--- src/build.sh
+++ src/build.sh
@@ -10,12 +10,17 @@ cc -std=c89 -DDEBUG -Wall -Wpedantic -Wshadow -Wuninitialized -Wextra -Werror=im
 # cc main.c -std=c89 -Os -DNDEBUG -g0 -s -Wall -o oscean
 
 # RPi
-# tcc -Wall oscean.c -o oscean
+# tcc -Wall main.c -o oscean
 
 # Plan9
-# pcc oscean.c -o oscean
+# pcc main.c -o oscean
 
-rm -f ../site/*
+rm -rf ../site/
+mkdir ../site
+
+# Valgrind
+# gcc -std=c89 -DDEBUG -Wall -Wpedantic -Wshadow -Wuninitialized -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -Wvla -g -Og main.c -o oscean
+# valgrind ./oscean
 
 ./oscean
 
