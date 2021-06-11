f07703c32f08d7be0c35b48cbc0eeff2fec9c567
neauoire <aliceffekt@gmail.com>
2021-06-11 07:33:45 -0700

    Better build

--- src/build.sh
+++ src/build.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
 
-# Lint
-clang-format -i main.c
-clang-format -i projects/marbles/marbles.c
+# Setup
+mkdir -p bin
 
 # Cleanup
-rm -f ./oscean
-rm -f ./marbles
 rm -rf ../site
 mkdir ../site
 
 # Projects
-cc projects/marbles/marbles.c -std=c89 -Os -DNDEBUG -g0 -s -Wall -Wno-unknown-pragmas -o marbles
+if [ "${1}" = '--projects' ]; 
+then
+	clang-format -i projects/marbles/marbles.c
+	rm -f ./bin/marbles
+	cc projects/marbles/marbles.c -std=c89 -Os -DNDEBUG -g0 -s -Wall -Wno-unknown-pragmas -o bin/marbles
+	./bin/marbles inc/html/death.htm
+fi
 
-# Linux(debug)
-cc -std=c89 -DDEBUG -Wall -Wno-unknown-pragmas -Wpedantic -Wshadow -Wuninitialized -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -Wvla -g -Og -fsanitize=address -fsanitize=undefined main.c -o oscean
-
-# Linux(fast)
-# cc main.c -std=c89 -Os -DNDEBUG -g0 -s -Wall -Wno-unknown-pragmas -o oscean
-
-# RPi
-# tcc -Wall main.c -o oscean
-
-# Plan9
-# pcc main.c -o oscean
-
-# Valgrind
-# gcc -std=c89 -DDEBUG -Wall -Wpedantic -Wshadow -Wuninitialized -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -Wvla -g -Og main.c -o oscean
-# valgrind ./oscean
-
-# Build Size
-# echo "$(du -b ./oscean | cut -f1) bytes written"
+# Linux
+if [ "${1}" = '--system' ]; 
+then
+	clang-format -i main.c
+	rm -f ./bin/oscean
+	cc -std=c89 -DDEBUG -Wall -Wno-unknown-pragmas -Wpedantic -Wshadow -Wuninitialized -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -Wvla -g -Og -fsanitize=address -fsanitize=undefined main.c -o bin/oscean
+fi
 
 # Run
-./marbles inc/html/death.htm
-./oscean
-
-# Cleanup
-rm -f ./oscean
-rm -f ./marbles
+./bin/oscean
