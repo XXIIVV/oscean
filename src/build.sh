240d5088fc38e2bb2814d307064363c2f2a196fa
neauoire <aliceffekt@gmail.com>
2022-02-19 14:59:38 -0800

    Started preprocessor

--- src/build.sh
+++ src/build.sh
@@ -1,9 +1,18 @@
 #!/bin/sh -e
 
-rm -f bin/oscean.rom
-rm -rf ../site
-mkdir -p ../site
+rm -rf bin
 mkdir -p bin
 
+# Pre-process
+
+rm -rf tmp
+mkdir -p tmp
+uxnasm maeve.tal bin/maeve.rom
+uxncli bin/maeve.rom
+
+# Create 
+
+rm -rf ../site
+mkdir -p ../site
 uxnasm oscean.tal bin/oscean.rom
 uxncli bin/oscean.rom
