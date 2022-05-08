8fd11b2e37dced3f2243e6ec7069d7fcd6a74e3d
neauoire <aliceffekt@gmail.com>
2022-05-08 16:53:24 -0700

    Added unicycle.rom

---
+++ etc/unicycle/emu.js
@@ -0,0 +1,94 @@
+'use strict'
+
+function Console(emu)
+{
+	this.buffer = ""
+	this.display = null
+
+	this.i = (char) => {
+		console.log("i",char)
+	}
+
+	this.send = (char) => {
+		if(char == 0x0a) {
+			this.display.innerHTML = this.buffer
+			this.buffer = ""
+		}
+		else{
+			this.buffer += String.fromCharCode(char)
+		}
+	}
+
+	this.input = (char) => {
+		// Get vector
+		let vec = emu.uxn.peek16(emu.uxn.dev + 0x10)
+		// Set char
+		emu.uxn.poke8(emu.uxn.dev + 0x12, char)
+		if(!vec)
+			console.warn("No console vector")
+		emu.uxn.eval(vec)
+	}
+}
+
+function Emu ()
+{
+	this.debug = 0
+	this.uxn = new Uxn(this)
+	this.console = new Console(this)
+
+	let opcodes = [
+		"LIT", "INC", "POP", "NIP", "SWP", "ROT", "DUP", "OVR",
+		"EQU", "NEQ", "GTH", "LTH", "JMP", "JCN", "JSR", "STH",
+		"LDZ", "STZ", "LDR", "STR", "LDA", "STA", "DEI", "DEO",
+		"ADD", "SUB", "MUL", "DIV", "AND", "ORA", "EOR", "SFT",
+		"BRK"]
+
+	function getname(byte) {
+		let m2 = !!(byte & 0x20) ? "2" : ""
+		let mr = !!(byte & 0x40) ? "r" : ""
+		let mk = !!(byte & 0x80) ? "k" : ""
+		return opcodes[byte & 0x1f] + m2 + mk + mr
+	}
+
+	this.debugger = () => {
+		if(!this.uxn.wst.ptr())
+			console.log("Stack is clean")
+		// Stack
+		let buf = ""
+		for (let i = 0; i < this.uxn.wst.ptr(); i++) {
+			buf += this.uxn.wst.get(i).toString(16)+" "
+		}
+		console.warn(buf)
+	}
+
+	this.onStep = (pc, instr) => {
+		if(this.debug)
+			console.log(getname(instr), pc)
+	}
+
+	this.dei = (port) => {
+		return this.uxn.getdev(port)
+	}
+
+	this.deo = (port, val) => {
+		this.uxn.setdev(port, val)
+		if(port == 0x10 || port == 0x11) {
+			console.log("Set console vector")
+		}
+		else if(port == 0x00 || port == 0x01) {
+			console.log("Set system vector")
+		}
+		else if(port == 0x02) {
+			this.uxn.wst.addr = val ? val * 0x100 : 0x10000
+		}
+		else if(port == 0x18) {
+			this.console.send(val)
+		}
+		else if(port == 0x0f) {
+			console.warn("Program ended.")
+		}
+		else {
+			console.log("Unknown deo", port, val)
+		}
+	}
+}
---
+++ etc/unicycle/uxn.js
@@ -0,0 +1,194 @@
+'use strict'
+
+function Stack(u, addr) 
+{
+	this.addr = addr
+
+	this.get = (index) => {
+		return u.ram[this.addr + index]
+	}
+
+	this.ptr = () => {
+		return this.get(0xff)
+	}
+
+	this.inc = () => {
+		return u.ram[this.addr + 0xff]++
+	}
+
+	this.dec = () => {
+		return u.rk ? --this.pk : --u.ram[this.addr + 0xff]
+	}
+
+	this.pop8 = () => {
+		return this.ptr() == 0x00 ? u.halt(1) : u.ram[this.addr + this.dec()]
+	}
+
+	this.push8 = (val) => {
+		if(this.ptr() == 0xff)
+			return u.halt(2)
+		u.ram[this.addr + this.inc()] = val
+	}
+
+	this.pop16 = () => {
+		return this.pop8() + (this.pop8() << 8)
+	}
+
+	this.push16 = (val) => {
+		this.push8(val >> 0x08)
+		this.push8(val & 0xff)
+	}
+}
+
+function Uxn (emu)
+{
+	this.ram = new Uint8Array(0x13000)
+	this.wst = new Stack(this, 0x10000)
+	this.rst = new Stack(this, 0x11000)
+	this.dev = 0x12000
+
+	this.getdev = (port) => { return this.ram[this.dev + port] }
+	this.setdev = (port, val) => { this.ram[this.dev + port] = val }
+
+	this.pop = () => {
+		return this.r2 ? this.src.pop16() : this.src.pop8()
+	}
+
+	this.push8 = (x) => {
+		this.src.push8(x)
+	}
+
+	this.push16 = (x) => {
+		this.src.push16(x)
+	}
+
+	this.push = (val) => {
+		if(this.r2)
+			this.push16(val)
+		else
+			this.push8(val)
+	}
+
+	this.peek8 = (addr) => {
+		return this.ram[addr]
+	}
+
+	this.peek16 = (addr) => {
+		return (this.ram[addr] << 8) + this.ram[addr + 1]
+	}
+
+	this.peek = (addr) => {
+		return this.r2 ? this.peek16(addr) : this.ram[addr]
+	}
+
+	this.poke8 = (addr, val) => {
+		this.ram[addr] = val
+	}
+
+	this.poke = (addr, val) => {
+		if(this.r2) {
+			this.ram[addr] = val >> 8;
+			this.ram[addr + 1] = val;
+		} else
+			this.ram[addr] = val
+	}
+
+	this.devr = (port) => {
+		return this.r2 ? (emu.dei(port) << 8) + emu.dei(port+1) : emu.dei(port)
+	}
+
+	this.devw = (port, val) => {
+		if(this.r2) {
+			emu.deo(port, val >> 8);
+			emu.deo(port+1, val & 0xff)
+		} else
+			emu.deo(port, val)
+	}
+
+	this.jump = (addr, pc) => {
+		return this.r2 ? addr : pc + rel(addr);
+	}
+
+	this.eval = (pc) => {
+		let a, b, c, instr
+		if(!pc || this.dev[0x0f])
+			return 0;
+		while((instr = this.ram[pc++])) {
+			emu.onStep(pc, instr)
+			// registers
+			this.r2 = instr & 0x20
+			this.rr = instr & 0x40
+			this.rk = instr & 0x80
+			if(this.rk) {
+				this.wst.pk = this.wst.ptr()
+				this.rst.pk = this.rst.ptr()
+			}
+			if(this.rr) {
+				this.src = this.rst
+				this.dst = this.wst
+			} else {
+				this.src = this.wst
+				this.dst = this.rst
+			}
+			switch(instr & 0x1f) {
+			// Stack
+			case 0x00: /* LIT */ this.push(this.peek(pc)); pc += !!this.r2 + 1; break;
+			case 0x01: /* INC */ this.push(this.pop() + 1); break;
+			case 0x02: /* POP */ this.pop(); break;
+			case 0x03: /* NIP */ a = this.pop(); this.pop(); this.push(a); break;
+			case 0x04: /* SWP */ a = this.pop(); b = this.pop(); this.push(a); this.push(b); break;
+			case 0x05: /* ROT */ a = this.pop(); b = this.pop(); c = this.pop(); this.push(b); this.push(a); this.push(c); break;
+			case 0x06: /* DUP */ a = this.pop(); this.push(a); this.push(a); break;
+			case 0x07: /* OVR */ a = this.pop(); b = this.pop(); this.push(b); this.push(a); this.push(b); break;
+			// Logic
+			case 0x08: /* EQU */ a = this.pop(); b = this.pop(); this.push8(b == a); break;
+			case 0x09: /* NEQ */ a = this.pop(); b = this.pop(); this.push8(b != a); break;
+			case 0x0a: /* GTH */ a = this.pop(); b = this.pop(); this.push8(b > a); break;
+			case 0x0b: /* LTH */ a = this.pop(); b = this.pop(); this.push8(b < a); break;
+			case 0x0c: /* JMP */ pc = this.jump(this.pop(), pc); break;
+			case 0x0d: /* JCN */ a = this.pop(); if(this.src.pop8()) pc = this.jump(a, pc); break;
+			case 0x0e: /* JSR */ this.dst.push16(pc); pc = this.jump(this.pop(), pc); break;
+			case 0x0f: /* STH */ if(this.r2){ this.dst.push16(this.src.pop16()); } else{ this.dst.push8(this.src.pop8()); } break;
+			// Memory
+			case 0x10: /* LDZ */ this.push(this.peek(this.src.pop8())); break;
+			case 0x11: /* STZ */ this.poke(this.src.pop8(), this.pop()); break;
+			case 0x12: /* LDR */ this.push(this.peek(pc + rel(this.src.pop8()))); break;
+			case 0x13: /* STR */ this.poke(pc + rel(this.src.pop8()), this.pop()); break;
+			case 0x14: /* LDA */ this.push(this.peek(this.src.pop16())); break;
+			case 0x15: /* STA */ this.poke(this.src.pop16(), this.pop()); break;
+			case 0x16: /* DEI */ this.push(this.devr(this.src.pop8())); break;
+			case 0x17: /* DEO */ this.devw(this.src.pop8(), this.pop()); break;
+			// Arithmetic
+			case 0x18: /* ADD */ a = this.pop(); b = this.pop(); this.push(b + a); break;
+			case 0x19: /* SUB */ a = this.pop(); b = this.pop(); this.push(b - a); break;
+			case 0x1a: /* MUL */ a = this.pop(); b = this.pop(); this.push(b * a); break;
+			case 0x1b: /* DIV */ a = this.pop(); b = this.pop(); if(!a) return this.halt(3); this.push(b / a); break;
+			case 0x1c: /* AND */ a = this.pop(); b = this.pop(); this.push(b & a); break;
+			case 0x1d: /* ORA */ a = this.pop(); b = this.pop(); this.push(b | a); break;
+			case 0x1e: /* EOR */ a = this.pop(); b = this.pop(); this.push(b ^ a); break;
+			case 0x1f: /* SFT */ a = this.src.pop8(); b = this.pop(); this.push(b >> (a & 0x0f) << ((a & 0xf0) >> 4)); break;
+			}
+		}
+	}
+
+	this.load = (program) => {
+		for (let i = 0; i <= program.length; i++)
+			this.ram[0x100 + i] = program[i];
+		return this
+	}
+
+	this.errors = [
+		"underflow",
+		"overflow",
+		"division by zero"
+	]
+
+	this.halt = (err) => {
+		console.error("Error", this.rr ? "Return-stack" : "Working-stack", this.errors[err]);
+		this.pc = 0x0000
+	}
+
+	function rel(val) {
+		return (val > 0x80 ? val - 256 : val)
+	}
+}
