f600e392af0d16873d689bef6d52c0f37ba0e528
neauoire <aliceffekt@gmail.com>
2022-09-09 08:12:54 -0700

    *

--- build.sh
+++ build.sh
@@ -1,25 +1,26 @@
 #!/bin/sh -e
 
+ASM="uxncli $HOME/roms/drifblim.rom"
+LIN="uxncli $HOME/roms/uxnlin.rom"
+
+if [[ "$*" == *"--lint"* ]]
+then
+	echo "Linting.."
+	$LIN src/maeve.tal
+	$LIN src/arvelie.tal
+	$LIN src/oscean.tal
+fi
+
 echo "Assembling -------------------"
 rm -rf bin
 mkdir -p bin
-
-if [ -e "$HOME/roms/uxnlin.rom" ]
-then
-	echo "Linting.."
-	uxncli $HOME/roms/uxnlin.rom src/maeve.tal
-	uxncli $HOME/roms/uxnlin.rom src/arvelie.tal
-	uxncli $HOME/roms/uxnlin.rom src/oscean.tal
-fi
-
-uxncli ~/roms/drifblim.rom src/arvelie.tal bin/arvelie.rom
-uxncli ~/roms/drifblim.rom src/maeve.tal bin/maeve.rom
-uxncli ~/roms/drifblim.rom src/oscean.tal bin/oscean.rom
+$ASM src/arvelie.tal bin/arvelie.rom
+$ASM src/maeve.tal bin/maeve.rom
+$ASM src/oscean.tal bin/oscean.rom
 
 echo "Parsing ----------------------"
 rm -rf tmp
 mkdir -p tmp
-
 uxncli bin/maeve.rom
 
 echo "Building ---------------------"
@@ -31,5 +32,7 @@ uxncli bin/arvelie.rom
 if [ "${1}" = '--push' ];
 then
 	echo "Pushing.."
-	git add * && git commit -m '*' && git push
+	git add *
+	git commit -m '*'
+	git push
 fi
