b54d30bb9143a0b8fc318a527264e4bcfb15c0f4
Devine Lu Linvega <aliceffekt@gmail.com>
2024-02-15 11:44:51 -0800

    *

--- etc/uxnrepl/build.sh
+++ etc/uxnrepl/build.sh
@@ -1,14 +1,9 @@
 #!/bin/sh -e
 
+cp ~/Git/drifblim/src/drifloon.tal etc/drifloon.tal
 
 uxnasm etc/format-js.tal format-js.rom
 uxnasm etc/drifloon.tal drifloon.rom
 uxncli format-js.rom drifloon.rom > src/asm.js
 
-# mkdir -p bin
-# uxnasm etc/format-js.tal bin/format-js.rom
-# uxnasm etc/drifloon.tal bin/drifloon.rom
-# uxncli bin/format-js.rom bin/drifloon.rom > src/asm.js
-
-# uxnasm etc/palette.tal bin/res.rom
-# uxncli bin/format-js.rom bin/res.rom
+rm *.rom
