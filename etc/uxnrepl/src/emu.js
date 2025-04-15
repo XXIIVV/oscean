29803c5d4b8dd1091702ee4f27593fa1e33308d8
Devine Lu Linvega <aliceffekt@gmail.com>
2025-04-14 19:33:46 -0700

    *

--- etc/uxnrepl/src/emu.js
+++ etc/uxnrepl/src/emu.js
@@ -1,7 +1,27 @@
 'use strict'
 
+function System(emu)
+{
+	this.vector = 0
+	this.meta = 0
+
+	this.deo = (port, val) => {
+		if(port == 0x00 || port == 0x01) 
+			this.vector = (emu.uxn.dev[0x00] << 8) | emu.uxn.dev[0x01]
+		else if(port == 0x06 || port == 0x07)
+			this.meta = (emu.uxn.dev[0x06] << 8) | emu.uxn.dev[0x07]
+		else if(port == 0x0e) 
+			console.log(this)
+		else if(port == 0x0f) 
+			console.log("Evaluation ended.")
+		else 
+			console.log("Unknown system port", port, val)
+	}
+}
+
 function Console(emu)
 {
+	this.vector = 0
 	this.write_el = null
 	this.error_el = null
 
@@ -16,13 +36,25 @@ function Console(emu)
 	this.input = (char, type) => {
 		emu.uxn.dev[0x17] = type
 		emu.uxn.dev[0x12] = char
-		emu.uxn.eval(emu.uxn.dev[0x10] << 8 | emu.uxn.dev[0x11])
+		emu.uxn.eval(this.vector)
+	}
+
+	this.deo = (port, val) => {
+		if(port == 0x10 || port == 0x11) 
+			this.vector = (emu.uxn.dev[0x10] << 8) | emu.uxn.dev[0x11]
+		else if(port == 0x18)
+			this.write(val)
+		else if(port == 0x19) 
+			this.error(val)
+		else 
+			console.log("Unknown console port", port, val)
 	}
 }
 
 function Emu ()
 {
 	this.uxn = new Uxn(this)
+	this.system = new System(this)
 	this.console = new Console(this)
 
 	this.dei = (port) => {
@@ -31,20 +63,10 @@ function Emu ()
 
 	this.deo = (port, val) => {
 		this.uxn.dev[port] = val
-		if(port == 0x00 || port == 0x01) {
-			console.log("Set system vector")
-		} else if(port == 0x0e) {
-			console.log(this)
-		} else if(port == 0x0f) {
-			console.log("Program ended.")
-		} else if(port == 0x10 || port == 0x11) {
-			console.log("Set console vector")
-		} else if(port == 0x18) {
-			this.console.write(val)
-		} else if(port == 0x19) {
-			this.console.error(val)
-		} else {
-			console.log("Unknown deo", port, val)
+		switch(port >> 4){
+			case 0: this.system.deo(port, val); break;
+			case 1: this.console.deo(port, val); break;
 		}
+		
 	}
 }
