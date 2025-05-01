8c7ddd6ca2281f57c52270e1242032570f4672f0
Devine Lu Linvega <aliceffekt@gmail.com>
2025-05-01 16:49:52 -0700

    *

--- etc/uxnrepl/src/examples.js
+++ etc/uxnrepl/src/examples.js
@@ -1,23 +1,6 @@
 /* Inspired from https://doc.rust-lang.org/rust-by-example/ */
 
 let examples = {
-
-"hello_tal": `|18 @Console/write
-
-|100 @on-reset ( -> )
-	;my-string print-text
-	BRK
-
-@print-text ( str* -- )
-	&while
-		LDAk .Console/write DEO
-		INC2 LDAk ?&while
-	POP2
-	JMP2r
-
-@my-string
-	"Hello 20 "World! 00`,
-
 /* 
 @|0.Hello-World */
 
@@ -41,7 +24,7 @@ let examples = {
 /* 
 @|1.Stack */
 
-"1_tal": `( The Uxn virtual machine has two stacks of 256 bytes.
+"1_tal": `( The Uxn virtual machine has a working stack of 256 bytes.
   A byte is a value between the hexadecimal numbers 00 and ff. )
 
 #0a          ( Push 0a byte to the stack )
@@ -49,9 +32,8 @@ let examples = {
 SWP          ( Swap them, so 0a is on top )
 NIP POP      ( Nip the 0b byte, pop the 0a byte )
 
-( There are four arithmetic operators.
-  Postfix arithmetic has no precedence rules,
-  operations are applied in the order. )
+( Postfix arithmetic has no precedence rules,
+  operations are simply applied in the order they are called. )
 
 #02 #10 ADD  ( 02 + 10 = 12 )
 #04 #08 SUB  ( 04 - 08 = fc )
@@ -64,9 +46,9 @@ NIP POP      ( Nip the 0b byte, pop the 0a byte )
 "2_tal": `( A literal is a number to be pushed to the stack,
   Uxntal supports various ways of creating literal bytes. )
 
-    80 01  ( A number will be interpreted as an opcode )
-   LIT 01  ( 80 is the numerical value of the LIT opcode )
-      #01  ( This is a shorthand for a literal byte )
+    80 12  ( A number will be interpreted as an opcode )
+   LIT 12  ( 80 is the numerical value of the LIT opcode )
+      #12  ( This is a shorthand for a literal byte )
 
 ( A literal short is a literal number made of two bytes. )
 
@@ -77,29 +59,26 @@ LIT2 1234  ( a0 is the numerical value of the LIT2 opcode )
 ( ASCII characters can also be made into literal bytes. )
 
 LIT "H #18 DEO ( Push the letter H, send to Console/write )
-LIT "i #18 DEO ( Push the letter i, send to Console/write )`,
+LIT2 "i 18 DEO ( Push the letter i, send to Console/write )`,
 
 /* 
-@|3.Enums&Structs */
+@|3.Functions */
 
-"3_tal": `( Programs can utilize up to ff00 of memory,
-  the program location is where the program data is written in memory. )
+"3_tal": `( Function labels are followed by a comment that explains
+the expected transformation, in the format: before -- after )
 
-|1234 ( Move program location to 1234 )
-$10   ( Move program location by 10, to 1244 )
+#12 double  ( Apply the "double" function on the number 12 )
+;promote    ( Push a pointer to the "promote" function )
+JSR2        ( Call the pointer to apply the "promote" function )
+BRK         ( Halt )
 
-( Scope in a program is created with a @label, 
-  members to this scope is created with &sublabels.  )
+@promote ( byte -- short* )
+  #00 SWP   ( Body of the function )
+  JMP2r     ( Return by jumping to address in return stack )
 
-|0  @enum &a $1 &b $1 &c   ( Enum with fields a=0, b=1, c=2 )
-|10 @struct &a $8 &b $2 &c ( Struct with fields a=10, b=18, c=1a )
-|200 @const                ( Const with a value of 200 )
-
-( By default, the program location begins at 100, 
-  but if the location has moved, it must be set back.
-  Every Uxn program begins at 100. )
-
-|100 .struct/b BRK`,
+@double ( value -- res )
+  DUP ADD
+  JMP2r`,
 
 /* 
 @|4.Variables */
@@ -124,22 +103,26 @@ BRK
 	@abs16 $2 `,
 
 /* 
-@|5.Functions */
+@|5.Enums&Structs */
 
-"5_tal": `( In Uxntal programs everything is a function 
-  doing some kind of transformation on stacks. )
+"5_tal": `( Programs can utilize up to ff00 of memory,
+  the program location is where the program data is written in memory. )
 
-#12 double  ( Apply the "double" function  )
-;promote    ( Push a pointer to the "promote function" )
-JSR2        ( Call the pointer and apply )
+|1234 ( Move program location to 1234 )
+$10   ( Move program location by 10, to 1244 )
 
-BRK         ( Halt )
+( Scope in a program is created with a @label, 
+  members to this scope is created with &sublabels.  )
 
-@promote ( byte -- short* )
-	#00 SWP JMP2r 
+|0  @enum &a $1 &b $1 &c   ( Enum with fields a=0, b=1, c=2 )
+|10 @struct &a $8 &b $2 &c ( Struct with fields a=10, b=18, c=1a )
+|200 @const                ( Const with a value of 200 )
 
-@double ( value -- res )
-	DUP ADD JMP2r`,
+( By default, the program location begins at 100, 
+  but if the location has moved, it must be set back.
+  Every Uxn program begins at 100. )
+
+|100 .struct/b BRK`,
 
 /* 
 @|6.If/Else */
