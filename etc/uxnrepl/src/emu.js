85f8d15cecdd20b9c8fb76ffc280eecb7e609759
Devine Lu Linvega <aliceffekt@gmail.com>
2025-07-31 16:03:08 -0700

    *

--- etc/uxnrepl/src/emu.js
+++ etc/uxnrepl/src/emu.js
@@ -64,15 +64,49 @@ function Console(emu)
 	}
 }
 
+
+function DateTime(emu)
+{
+	this.dei = (port) => {
+		const now = new Date();
+		switch (port) {
+			case 0xc0: return now.getFullYear() >> 8;
+			case 0xc1: return now.getFullYear() & 0xff;
+			case 0xc2: return now.getMonth();
+			case 0xc3: return now.getDate();
+			case 0xc4: return now.getHours();
+			case 0xc5: return now.getMinutes();
+			case 0xc6: return now.getSeconds();
+			case 0xc8: return doty() >> 8;
+			case 0xc9: return doty() & 0xff;
+			// TODO dst
+			// case 0xca https://stackoverflow.com/a/56429156
+		}
+		return 1;
+	}
+
+	function doty() {
+		let now = new Date()
+		let start = new Date(now.getFullYear(), 0, 0)
+		let diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000)
+		let oneDay = 1000 * 60 * 60 * 24
+		return Math.floor(diff / oneDay) - 1
+	}
+}
+
+
 function Emu ()
 {
 	this.uxn = new Uxn(this)
 	this.system = new System(this)
 	this.console = new Console(this)
+	this.datetime = new DateTime(this)
 
 	this.dei = (port) => {
-		if(port >> 4 == 0)
+		if(port >> 4 == 0x0)
 			return this.system.dei(port);
+		if(port >> 4 == 0xc)
+			return this.datetime.dei(port);
 		return this.uxn.dev[port]
 	}
 
