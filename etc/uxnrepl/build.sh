a26ffd5ef1a9d759509191b2bd963de70d3376e8
Devine Lu Linvega <aliceffekt@gmail.com>
2025-10-22 13:29:43 -0700

    *

--- etc/uxnrepl/build.sh
+++ etc/uxnrepl/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
-uxnasm ../format-js.tal.txt format-js.rom
-uxnasm ../drifloon.tal.txt assembler
+uxncli ~/roms/drifblim.rom ../format-js.tal.txt format-js.rom
+uxncli ~/roms/drifblim.rom ../drifloon.tal.txt assembler
 uxncli format-js.rom assembler > src/asm.js
 
 rm -f format-js.rom assembler
