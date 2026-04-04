916c9773a592ac609bad02d9dc55c03f696e2b0c
Devine Lu Linvega <aliceffekt@gmail.com>
2026-04-03 20:12:51 -0700

    *

--- etc/rejoicerepl/src/examples.js
+++ etc/rejoicerepl/src/examples.js
@@ -15,6 +15,26 @@ let examples = {
 
 "fizzbuzz": 
 
-`TODO`
+`TODO`,
 
 }
+
+examples.binary = `( A short example to demonstrate the result 
+	of some binary logic operations )
+
+not true
+
+	false/[not true]
+	true/[not false]
+
+and true true
+
+	true/[and true true]
+	false/[and true]
+	false/[and]
+
+or true true
+
+	true/[or true true]
+	true/[or true]
+	false/[or]`
