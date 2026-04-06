5be0776b45037b5410f72b4f07bf7213df182490
Devine Lu Linvega <aliceffekt@gmail.com>
2026-04-05 20:37:54 -0700

    *

--- etc/rejoicerepl/src/examples.js
+++ etc/rejoicerepl/src/examples.js
@@ -21,8 +21,8 @@ examples.primes = `( Multiplying is the same as adding prime factors.
 
 10 21
 
-( Division is the same as subtracting prime factors. 
-	Multiplying by the fraction 1/3, is the same as dividing by 3. 
+( Division is the same as subtracting prime factors.
+	Multiplying by the fraction 1/3, is the same as dividing by 3.
 	And the result is the subtraction of the prime factor 3. )
 
 1/3
@@ -47,7 +47,7 @@ examples.fractions = `( A whole number, equal to 7/1: )
 examples.postfix = `( .. )
 `
 
-examples.tropical = `( In Tropical Arithmetic, the multiplication operation is addition, 
+examples.tropical = `( In Tropical Arithmetic, the multiplication operation is addition,
 	akin to how a product is the addition prime factors.
 
  × 153
@@ -58,23 +58,51 @@ examples.tropical = `( In Tropical Arithmetic, the multiplication operation is a
 [a b^5 c^3] [b^6 c]
 `
 
-examples.product = `( A program to multiply two numbers 
+examples.product = `( A program to multiply two numbers
 	and print the result: )
 
 x^2 y^3
 @Mul ( x y -- res )
-	[Mul z res]/y
-@Move ( z -- y)
-	[Move y]/z
+	[Mul t res]/y
+@MulMov
+	[MulMov y]/t
 	Mul/x
-@Clean ( y -- )
-	Clean/[y res]
+@MulClean
+	MulClean/[y res]
 
 .#res`
 
-examples.fizzbuzz = `( Prints numbers from 1 to 100, 
+examples.quotient = `( A program to divides a number by another
+	and print the result: )
+
+x^24 y^3
+@Div ( x y -- res )
+	[Div t]/[y x] res
+@DivMov
+	[DivMov y]/t
+	[Div x]/x
+@DivClean
+	DivClean/y
+
+.#res`
+
+examples.gcd = `( A program to find the GCD of two numbers,
+	and print the result: )
+
+x^6 y^15
+@Gcd ( x y -- x )
+	[Gcd z]/[x y]
+@GcdSwap
+	[GcdSwap y]/x
+@GcdRestore
+	[GcdRestore x]/z
+	[Gcd y]/y
+
+.#x`
+
+examples.fizzbuzz = `( Prints numbers from 1 to 100,
 	replacing multiples of 3 with Fizz,
-	multiples of 5 with Buzz, 
+	multiples of 5 with Buzz,
 	and multiples of both with FizzBuzz )
 
 times^100 f b
@@ -92,26 +120,34 @@ examples.colors = `red^2 blue^2 yellow^2
 	black/[violet green orange]
 `
 
-examples.gcd = `( A program to find the GCD of two numbers,
-	and print the result: )
+examples.spirograph = `( inner cog: ) x^40
+( outer cog  ) a^96
 
-x^6 y^15
+@Copy ( a -- y y2 )
+	[Copy y y2]/a
+
 @Gcd ( x y -- x )
 	[Gcd z]/[x y]
-@Swap
-	[Swap y]/x
-@Restore
-	[Restore x]/z
-	[Gcd y]/y
+@GcdSwap
+	[GcdSwap y]/x
+@GcdRestore
+	[GcdRestore x]/z [Gcd y]/y
 
-.#x`
+@Div ( y2 x -- res )
+	[Div t]/[y2 x] res
+@DivMov
+	[DivMov x]/t [Div y2]/y2
+@DivClean
+	DivClean/x
+
+.Turns:\\s .#res`
 
 
 /*
 @|Binary */
 
 
-examples.binary_logic = `( An example to demonstrate the result 
+examples.binary_logic = `( An example to demonstrate the result
 	of some binary logic operations )
 
 not true
@@ -134,12 +170,12 @@ n^73 n^124
 
 ( Decimal to Binary )
 
-@B7 [B7 b7]/n^128 @B6 [B6 b6]/n^64 @B5 [B5 b5]/n^32 @B4 [B4 b4]/n^16 
+@B7 [B7 b7]/n^128 @B6 [B6 b6]/n^64 @B5 [B5 b5]/n^32 @B4 [B4 b4]/n^16
 @B3 [B3 b3]/n^8   @B2 [B2 b2]/n^4  @B1 [B1 b1]/n^2  @B0 [B0 b0]/n
 
 ( Adder with Carry )
 
-          b7/[b6^2] b6/[b5^2] b5/[b4^2]  
+          b7/[b6^2] b6/[b5^2] b5/[b4^2]
 b4/[b3^2] b3/[b2^2] b2/[b1^2] b1/[b0^2]
 
 ( Binary to Decimal )
