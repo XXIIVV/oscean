80756387a05d5735513ecabfab4064c1265b7594
Devine Lu Linvega <aliceffekt@gmail.com>
2026-04-27 11:00:21 -0700

    *

--- etc/uxnrepl/src/emu.js
+++ etc/uxnrepl/src/emu.js
@@ -13,15 +13,15 @@ function System(emu)
 			res += ('0' + stk[i & 0xff].toString(16)).slice(-2)
 			res += ((i + 1) & 0xff) ? ' ' : '|'
 		}
-		return res;
+		return res
 	}
 
 	this.print_wst = () => {
-		return print_stack(0);
+		return print_stack(0)
 	}
 
 	this.print_rst = () => {
-		return print_stack(1);
+		return print_stack(1)
 	}
 }
 
@@ -93,18 +93,18 @@ function Emu ()
 		switch(port){
 		/* System */
 		case 0x00:
-		case 0x01: this.system.vector = (this.uxn.dev[0x00] << 8) | this.uxn.dev[0x01]; break;
+		case 0x01: this.system.vector = (this.uxn.dev[0x00] << 8) | this.uxn.dev[0x01]; break
 		case 0x06:
-		case 0x07: this.system.meta = (this.uxn.dev[0x06] << 8) | this.uxn.dev[0x07]; break;
-		case 0x0e: this.console.write_string(`${this.system.print_wst()}\n${this.system.print_rst()}`); break;
-		case 0x0f: console.log("Evaluation ended."); break;
+		case 0x07: this.system.meta = (this.uxn.dev[0x06] << 8) | this.uxn.dev[0x07]; break
+		case 0x0e: this.console.write_string(`${this.system.print_wst()}\n${this.system.print_rst()}`); break
+		case 0x0f: console.log("Evaluation ended."); break
 		/* Console */
 		case 0x10:
-		case 0x11: this.console.vector = (this.uxn.dev[0x10] << 8) | this.uxn.dev[0x11]; break;
-		case 0x18: this.console.write(val); break;
-		case 0x19: this.console.error(val); break;
-		case 0x1a: this.console.error_byte(val); break;
-		case 0x1b: this.console.error_byte(val); break;
+		case 0x11: this.console.vector = (this.uxn.dev[0x10] << 8) | this.uxn.dev[0x11]; break
+		case 0x18: this.console.write(val); break
+		case 0x19: this.console.error(val); break
+		case 0x1a: this.console.error_byte(val); break
+		case 0x1b: this.console.error_byte(val); break
 		}
 	}
 }
@@ -127,16 +127,15 @@ function Repl(rom, keyword)
 			let { value, selectionStart, selectionEnd } = this.editor_el
 			if (keyCode === 9) { // TAB = 9
 				e.preventDefault()
-				this.editor_el.value = value.slice(0, selectionStart) + "\t" + value.slice(selectionEnd);
+				this.editor_el.value = value.slice(0, selectionStart) + "\t" + value.slice(selectionEnd)
 				this.editor_el.setSelectionRange(selectionStart+1, selectionStart+1)
 			}
-			else if (e.ctrlKey && e.key === 'Enter') 
-				this.run();
-		});
+			else if (e.ctrlKey && e.key === 'Enter')
+				this.run()
+		})
 		this.run_el.addEventListener("click", this.run)
 		this.run_el.value = keyword
 		document.body.className = "active"
-		setTimeout(() => { this.editor_el.focus() }, 0);	
 	}
 
 	this.load_examples = () => {
@@ -148,30 +147,31 @@ function Repl(rom, keyword)
 				.map(word => word.charAt(0).toUpperCase() + word.slice(1))
 				.join(' ')
 			this.examples_el.add(new Option(name, key))
-		});
+		})
 		// Load example from hash
 		const choice = window.location.hash.slice(1)
 		if(examples[choice]) {
-			for (let i = 0; i < this.examples_el.options.length; i++) 
-				if (this.examples_el.options[i].value === choice) 
-					this.examples_el.selectedIndex = i;
+			for (let i = 0; i < this.examples_el.options.length; i++)
+				if (this.examples_el.options[i].value === choice)
+					this.examples_el.selectedIndex = i
 			this.select_example(choice)
 		} else
 			this.select_example(0)
 		// Connect examples
 		this.examples_el.addEventListener('change', (e) => {
 			this.select_example(e.currentTarget.value)
-		}, true);
+		}, true)
 	}
 
 	this.popup = (text) => {
-		const popup = window.open('', 'Popup', 'width=400,height=300');
-		popup.document.body.innerHTML = `<pre>${text}</pre>`;
+		const popup = window.open('', 'Popup', 'width=400,height=300')
+		popup.document.body.innerHTML = `<pre>${text}</pre>`
 	}
 
 	this.select_example = (value) => {
 		this.editor_el.value = value ? examples[value] : examples.hello_world
 		this.logs_el.innerHTML = `Press <b>${keyword}</b> to evaluate.`
+		this.editor_el.focus()
 	}
 
 	this.run = () => {
@@ -190,6 +190,7 @@ function Repl(rom, keyword)
 			this.result_el.innerHTML = emu.console.stdout_body
 		// Trim console
 		this.logs_el.innerHTML = emu.console.stderr_body.trim().split('\n').slice(-50).join('\n')
-		this.logs_el.scrollTop = this.logs_el.scrollHeight;
+		this.logs_el.scrollTop = this.logs_el.scrollHeight
+		this.editor_el.focus()
 	}
 }
