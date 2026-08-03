9aaf0b2e99ec53d16637ed7875c4045152439e00
Devine Lu Linvega <aliceffekt@gmail.com>
2026-08-03 15:56:56 -0700

    *

--- etc/pop2repl/src/examples.js
+++ etc/pop2repl/src/examples.js
@@ -1,12 +1,33 @@
 let examples={}
-examples.hello_world=`function printHello;
-	vars text:"Hello World!" cx:1 ch:2 i;
+examples.hello_world=`comment Welcome to POP-2;
+
+function printString s;
+	vars cx:1 ch:2 i;
 	0 -> i;
 loop:
-	text{i} -> ch;
+	{s+i} -> ch;
 	if cx; then cx => 23, i + 1 -> i, goto loop;
 	close
 end
 
-printHello();
+vars text:"Hello World!";
+printString(#text);
+`
+examples.factorial=`comment Factorial, recursive;
+function factRec n;
+	if n = 0; then 1;
+	else n * factRec(n-1);
+	close
+end
+factRec(5);
+
+comment Factorial, tailcalls;
+function fact n;
+	vars p; 1 => p;
+loop:
+	if n = 0; then p;
+	else n*p => p; n-1 => n; goto loop
+	close
+end
+fact(5);
 `
